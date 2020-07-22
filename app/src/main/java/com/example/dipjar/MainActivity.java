3dbe20e5dde2efbec6f354a855574e4396e48fdc
jkairoy <jkairoy@gmail.com>
2020-07-22 11:15:31 -0700

    failure #2, working directly with okhttp

--- app/src/main/java/com/example/dipjar/MainActivity.java
+++ app/src/main/java/com/example/dipjar/MainActivity.java
@@ -11,11 +11,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 
-import pojos.SwipeResponse;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.tls.HandshakeCertificates;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,8 +37,7 @@ public class MainActivity extends AppCompatActivity {
                 text.append('\n');
             }
             br.close();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             Log.i("err", e.toString());
         }
         return text.toString();
@@ -50,27 +54,53 @@ public class MainActivity extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                //Request a string response from the URL resource
-//                SSLSocketFactory fac = new CrtSocketFactory().SocketFactory(getApplicationContext());
-//                try {
-//                    HttpsURLConnection connection = (HttpsURLConnection) new URL(url).openConnection();
-//                    connection.setSSLSocketFactory(fac);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//                Log.i("HELLO", fac.toString());
-                final SwipeResponse swipe = new SwipeResponse(testCard());
-                Call<SwipeResponse> swipeCall = apiInterface.swipeCallback(swipe);
-                swipeCall.enqueue(new Callback<SwipeResponse>() {
+                OkHttpClient client;
+                CertificateFactory certFactory = null;
+                X509Certificate cert = null;
+                InputStream certraw = getResources().openRawResource(R.raw.djca);
+                try {
+                    certFactory = CertificateFactory.getInstance("X.509");
+                    cert = (X509Certificate) certFactory.generateCertificate(certraw);
+                } catch (CertificateException e) {
+                    e.printStackTrace();
+                }
+                HandshakeCertificates certificates = new HandshakeCertificates.Builder()
+                        .addTrustedCertificate(cert).build();
+                client = new OkHttpClient.Builder()
+                        .sslSocketFactory(certificates.sslSocketFactory(), certificates.trustManager())
+                        .build();
+                Request request = new Request.Builder()
+                        .url("https://tx-stage.djsvc.net")
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
                     @Override
-                    public void onResponse(Call<SwipeResponse> call, Response<SwipeResponse> response) {
-                        Log.i("OK", response.toString());
+                    public void onFailure(Call call, IOException e) {
+                        Log.i("OOP", "aw man");
+                        e.printStackTrace();
                     }
+
                     @Override
-                    public void onFailure(Call<SwipeResponse> call, Throwable t) {
-                        Log.i("OHNO", t.toString());
+                    public void onResponse(Call call, final Response response) throws IOException {
+                        if (!response.isSuccessful()) {
+                            throw new IOException("Unexpected code " + response);
+                        } else {
+                            // do something wih the result
+                        }
                     }
                 });
+//                final SwipeResponse swipe = new SwipeResponse(testCard());
+//                Call<SwipeResponse> swipeCall = apiInterface.swipeCallback(swipe);
+//                swipeCall.enqueue(new Callback<SwipeResponse>() {
+//                    @Overrides
+//                    public void onResponse(Call<SwipeResponse> call, Response<SwipeResponse> response) {
+//                        Log.i("OK", response.toString());
+//                    }
+//                    @Override
+//                    public void onFailure(Call<SwipeResponse> call, Throwable t) {
+//                        Log.i("OHNO", t.toString());
+//                    }
+//                });
             }
         });
     }
